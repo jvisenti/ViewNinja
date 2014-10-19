4ad3ab85217669d9cbcd1859f0d6b53c6be41482
Rob Visentin <jvisenti@gmail.com>
2014-10-19 17:23:16 -0400

    added demo project

---
+++ RZNinjaViewDemo/RZNinjaViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RZNinjaViewDemo
+//
+//  Created by Rob Visentin on 10/19/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
